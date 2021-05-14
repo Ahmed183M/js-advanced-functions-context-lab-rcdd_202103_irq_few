80695fbbb2f3ac0db23add35c86630498f7c38d7
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-05-14 18:56:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ function hoursWorkedOnDate(date){
 }
 
 function wagesEarnedOnDate(date){
-  return hoursWorkedOnDate(this.date.timeInEvents.find(dateNow=>dateNow==date))*this.payPerHour;
+  return hoursWorkedOnDate.bind(date)*this.payPerHour;
 }
 
 function findEmployeeByFirstName(srcArray, firstName){
